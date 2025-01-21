61523d7c413ae3fc60306be4d3895deb0a8f81b8
Aravind Gopinath Nair <agn7679@gmail.com>
2025-01-21 23:04:00 +0800

    Added file upload feature

--- src/components/chatsColumn.js
+++ src/components/chatsColumn.js
@@ -1,12 +1,42 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import ChatPreview from "./chatPreview";
 import AddIcon from "./../add.png";
 import Upload from "./../upload.png";
 import TitleModal from "./titleModal";
+import FileUpload from "./fileUpload";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
   
 
 export default function ChatsColumn({ chatPreviews, handleChatChange, addChat, onDeleteChat }) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [fileShown, setFileShown] = useState(false);
+    const [fileList, setFileList] = useState([])
+
+    // auth header to authenticate user backend requests
+    const authHeader = useAuthHeader();
+
+     // get file list on startup
+     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fileList = await axios.get(
+                    'http://localhost:3001/file',
+                    {
+                        headers: {'Authorization':authHeader},
+                    }
+                );
+                if (fileList && fileList.data) {
+                    setFileList(fileList.data)
+                } else {
+                    console.error("No data received");
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const handleContextMenu = (e, id) => {
         e.preventDefault(); // Prevent the default context menu from appearing
@@ -27,22 +57,32 @@ export default function ChatsColumn({ chatPreviews, handleChatChange, addChat, o
                     <img src={AddIcon} alt="add" style={{ height: 24 }} />
                     <p className="newchattext">New Chat</p>
                 </button>
-                <button className="chatbuttons">
+                <button className="chatbuttons" onClick={() => setFileShown(prev=>!prev)}>
                     <img src={Upload} alt="add" style={{ height: 24 }} />
                     <p className="newchattext">Add Files</p>
                 </button>
             </div>
             
-            {chatPreviews.map((msg, index) => (
-                <button onClick={async()=>{handleChatChange(msg.id)}} key={index} 
-                onContextMenu={(e)=>handleContextMenu(e,msg.id)}>
-                    <ChatPreview 
-                        title={msg.title}
-                        lastMessage={msg.lastMessage}
-                        timeStamp={msg.timeStamp}
-                    />
-                </button>
-            ))}
+            {fileShown ? (
+                   <>
+                   <FileUpload setFileList={setFileList}/>
+                   {fileList.map((file, index) => (
+                       <div key={index}>{file}</div>
+                   ))}
+               </>
+               ) : 
+                (chatPreviews.map((msg, index) => (
+                    <button onClick={async()=>{handleChatChange(msg.id)}} key={index} 
+                    onContextMenu={(e)=>handleContextMenu(e,msg.id)}>
+                        <ChatPreview 
+                            title={msg.title}
+                            lastMessage={msg.lastMessage}
+                            timeStamp={msg.timeStamp}
+                        />
+                    </button>
+                )))
+               }
+            
             
         </div>
     );
---
+++ src/components/fileUpload.js
@@ -0,0 +1,40 @@
+import React, { useState} from 'react';
+import axios from 'axios';
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
+
+export default function FileUpload({setFileList}) {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    // auth header to authenticate user backend requests
+    const authHeader = useAuthHeader();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('pdf', file);
+        
+        try {
+            const response = await axios.post('http://localhost:3001/file/scan', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': authHeader
+                },
+            });
+            setFileList(files => [...files, response.data.filename])
+            console.log(response.data)
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="file" accept="application/pdf" onChange={handleFileChange} />
+            <button type="submit">Upload PDF</button>
+        </form>
+    );
+};
