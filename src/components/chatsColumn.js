610be6bacc2d29d0e3a15640cc3d54ed4f7a5702
Aravind Gopinath Nair <agn7679@gmail.com>
2025-02-22 10:40:34 +0800

    Added multiple file upload and delete, with updated styles

--- src/components/chatsColumn.js
+++ src/components/chatsColumn.js
@@ -1,45 +1,15 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import React, {useState} from "react";
 import ChatPreview from "./chatPreview";
 import AddIcon from "./../add.png";
 import Upload from "./../upload.png";
 import TitleModal from "./titleModal";
 import FileUpload from "./fileUpload";
-import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
   
 
 export default function ChatsColumn({ chatPreviews, handleChatChange, addChat, onDeleteChat }) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const [fileShown, setFileShown] = useState(false);
-    const [fileList, setFileList] = useState([])
 
-    // auth header to authenticate user backend requests
-    const authHeader = useAuthHeader();
-    const backend = process.env.REACT_APP_BACKEND_URL
-
-    const headers = {
-        Authorization: authHeader,
-    };
-
-     // get file list on startup
-     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const fileList = await axios.get(
-                    `${backend}/upload/`,
-                    {headers}
-                );
-                if (fileList && fileList.data) {
-                    setFileList(fileList.data)
-                } else {
-                    console.error("No data received");
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-        fetchData();
-    }, []);
 
     const handleContextMenuChat = (e, id) => {
         e.preventDefault(); // Prevent the default context menu from appearing
@@ -47,18 +17,6 @@ export default function ChatsColumn({ chatPreviews, handleChatChange, addChat, o
             onDeleteChat(id); // Call the onDeleteChat function with the chat ID
         }
     };
-
-    const handleContextMenuFile = async(e, filename) => {
-        e.preventDefault(); // Prevent the default context menu from appearing
-        if (window.confirm('Do you want to delete this file?')) {
-            
-            await axios.delete(
-                `${backend}/upload/${filename}`,
-                {headers}
-            )
-            setFileList(fileList.filter((file) => file !== filename));
-        }
-    }
 
     return (
         <div className="h-full mt-12 flex flex-col">
@@ -70,36 +28,33 @@ export default function ChatsColumn({ chatPreviews, handleChatChange, addChat, o
                 />
                 <button className="flex bg-white m-4 py-4 px-6" onClick={() => setIsDialogOpen(true)}>
                     <img src={AddIcon} alt="add" style={{ height: 24 }} />
-                    <p className="newchattext">New Chat</p>
+                    <p >New Chat</p>
                 </button>
                 <button className="flex bg-white m-4 py-4 px-6" onClick={() => setFileShown(prev=>!prev)}>
                     <img src={Upload} alt="add" style={{ height: 24 }} />
-                    <p className="newchattext">Add Files</p>
+                    <p >Add Files</p>
                 </button>
             </div>
             
-            <div className="flex flex-col h-full overflow-hidden hover:overflow-y-auto ">
-            {fileShown ? (
-                   <>
-                   <FileUpload setFileList={setFileList}/>
-                   {fileList.map((file, index) => (
-                       <div key={index} onContextMenu={(e)=>handleContextMenuFile(e, file)}>{file}</div>
-                   ))}
-               </>
-               ) : 
-                (chatPreviews.map((chat, index) => (
-                    <button 
-                        onClick={async()=>{handleChatChange(chat.id)}} key={index} 
-                        onContextMenu={(e)=>handleContextMenuChat(e,chat.id)}
-                    >
-                        <ChatPreview 
-                            title={chat.title}
-                            lastMessage={chat.lastMessage}
-                            timeStamp={chat.timeStamp}
-                        />
-                    </button>
-                )))
-               }
+            <div className=" flex flex-col overflow-hidden">
+                {fileShown ? 
+                    <FileUpload/>
+                : 
+                <div className="flex flex-col overflow-hidden hover:overflow-y-auto">
+                    {chatPreviews.map((chat, index) => (
+                        <button 
+                            onClick={async()=>{handleChatChange(chat.id)}} key={index} 
+                            onContextMenu={(e)=>handleContextMenuChat(e,chat.id)}
+                        >
+                            <ChatPreview 
+                                title={chat.title}
+                                lastMessage={chat.lastMessage}
+                                timeStamp={chat.timeStamp}
+                            />
+                        </button>
+                    ))}
+                </div>
+                }
             </div>
             
         </div>
--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -1,61 +1,199 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
-import {LineWave} from 'react-loader-spinner'
+import { LineWave } from 'react-loader-spinner';
 
-export default function FileUpload({setFileList}) {
-    const [file, setFile] = useState(null);
-    const [loading, setLoading] = useState(false)
+export default function FileUpload() {
+    const [files, setFiles] = useState([]);
+    const [sectors, setSectors] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [fileList, setFileList] = useState([]);
+
+    const [isInputMode, setIsInputMode] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [selectedSector, setSelectedSector] = useState('');
 
     const handleFileChange = (e) => {
-        setFile(e.target.files[0]);
+        setFiles([...e.target.files]); // Collect all selected files
     };
 
-    // auth header to authenticate user backend requests
     const authHeader = useAuthHeader();
-    
-    const backend = process.env.REACT_APP_BACKEND_URL
+    const headers = {
+        Authorization: authHeader,
+    };
+    const backend = process.env.REACT_APP_BACKEND_URL;
+
+    // get file list on startup
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fileList = await axios.get(
+                    `${backend}/upload/`,
+                    {headers}
+                );
+                if (fileList && fileList.data) {
+                    setFileList(fileList.data)
+                } else {
+                    console.error("No data received");
+                }
+
+                const sectorList = await axios.get(
+                    `${backend}/upload/sectors`,
+                    {headers}
+                );
+                if (sectorList && sectorList.data) {
+                    setSectors(sectorList.data)
+                } else {
+                    console.error("No data received");
+                }
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleToggle = () => {
+        setIsInputMode(!isInputMode);
+        if (isInputMode) {
+            setInputValue(''); // Clear input when switching to dropdown
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleSelectChange = (e) => {
+        setSelectedSector(e.target.value);
+    };
+
+    const handleAddOption = () => {
+        if (inputValue && !sectors.includes(inputValue)) {
+            setSectors([...sectors, inputValue]);
+            setSelectedSector(inputValue); // Set the new option as selected
+            setInputValue(''); // Clear input after adding
+        }
+    };
+
+    const handleContextMenuFile = async(e, selectedFile) => {
+        e.preventDefault(); // Prevent the default context menu from appearing
+        if (window.confirm('Do you want to delete this file?')) {
+            
+            await axios.delete(
+                `${backend}/upload/${selectedFile.sector}/${selectedFile.file}`,
+                {headers}
+            )
+            // update useState file list
+            setFileList(fileList.filter((file) => file.file !== selectedFile.file));
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append('pdf', file);
         
+        // Append each selected file to formData
+        Array.from(files).forEach(file => {
+            formData.append('pdf', file);
+        });
+
+        // Append sector attribute of chosen files
+        formData.append('sector', selectedSector);
+
         try {
-            setLoading(true)
-            const response = await axios.post(`${backend}/upload/scan`, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'Authorization': authHeader
-                },
-            });
-            setLoading(false)
-            setFileList(files => [...files, response.data.filename])
+            setLoading(true);
+            const response = await axios.post(`${backend}/upload/scan`, formData, {headers});
+            setLoading(false);
+            // Assuming the response contains an array of filenames
+            setFileList(prevFiles => [...prevFiles, ...response.data.filenames]);
         } catch (error) {
-            console.error('Error uploading file:', error);
+            console.error('Error uploading files:', error);
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="file" accept="application/pdf" onChange={handleFileChange} />
-            {loading?
-                <LineWave
-                    visible={true}
-                    height="100"
-                    width="100"
-                    color="#4fa94d"
-                    ariaLabel="line-wave-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                    firstLineColor=""
-                    middleLineColor=""
-                    lastLineColor=""
+        <div className="flex flex-col h-full">
+            <div className="flex ml-5 border border-gray-300 rounded-full overflow-hidden h-10 w-60">
+                <div 
+                    className={`flex-1 text-center py-2 cursor-pointer ${!isInputMode ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`} 
+                    onClick={handleToggle}
+                >
+                    Select Sector
+                </div>
+                <div 
+                    className={`flex-1 text-center py-2 cursor-pointer ${isInputMode ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`} 
+                    onClick={handleToggle}
+                >
+                    New Sector
+                </div>
+            </div>
+
+            {isInputMode ? (
+                <div className="mt-4">
+                    <input 
+                        type="text" 
+                        value={inputValue} 
+                        onChange={handleInputChange} 
+                        placeholder="Enter new option" 
+                        className="border border-gray-300 rounded p-2"
+                    />
+                    <button 
+                        onClick={handleAddOption} 
+                        className="ml-2 bg-blue-500 text-white rounded px-4 py-2"
+                    >
+                        Add Option
+                    </button>
+                </div>
+            ) : (
+                <select 
+                    value={selectedSector} 
+                    onChange={handleSelectChange} 
+                    className="mt-4 border border-gray-300 rounded p-2"
+                >
+                    <option value="">Select a sector</option>
+                    {sectors.map((sector, index) => (
+                        <option key={index} value={sector}>
+                            {sector}
+                        </option>
+                    ))}
+                </select>
+            )}
+
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="file" 
+                    className="block w-full text-sm text-gray-500 
+                                file:mr-4 file:py-2 file:px-4 
+                                file:rounded file:border file:border-gray-300 
+                                file:text-sm file:font-semibold 
+                                file:bg-blue-100 file:text-black 
+                                hover:file:bg-blue-100 transition duration-200 
+                                focus:outline-none focus:ring-2 focus:border-transparent"
+                    accept="application/pdf" 
+                    onChange={handleFileChange} 
+                    multiple // Allow multiple files
                 />
-            :
-            <button type="submit">Upload PDF</button>
-            }
-            
-        </form>
+                {loading ? (
+                    <LineWave
+                        visible={true}
+                        height="100"
+                        width="100"
+                        color="#4fa94d"
+                        ariaLabel="line-wave-loading"
+                    />
+                ) : (
+                    <button type="submit">Upload PDFs</button>
+                )}
+            </form>
+            <div className = "flex flex-col h-80 bg-white shadow-md rounded-lg divide-y divide-gray-200 overflow-hidden hover:overflow-y-auto">
+                {fileList.map((file, index) => (
+                        <div key={index} className="p-4 hover:bg-gray-100 transition" onContextMenu={(e)=>handleContextMenuFile(e, file)}>{file.file}</div>
+                    ))}
+            </div>
+        </div>
+        
+        
     );
-};
+}
